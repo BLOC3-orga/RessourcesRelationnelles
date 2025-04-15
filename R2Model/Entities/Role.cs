abef380d9acf8f76ad9a3399b949911d4724426f
DavidLaclef <daev.cognita@gmail.com>
2025-04-15 14:58:43 +0200

    Initialisation développement

---
+++ R2Model/Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace R2Model.Entities;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string Label { get; set; } = null!;
+
+    public ICollection<UserRight>? UserRights { get; set; } = null!;
+}
---
+++ R2Model/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace R2Model.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Pseudo { get; set; } = null!;
+    public string City { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public bool IsAccountActivated { get; set; } = true;
+
+    [ForeignKey(nameof(Role))]
+    public int RoleId { get; set; }
+    public virtual Role? Role { get; set; }
+
+    // ajouter les attributs liés aux autres entités (ex: List<Ressource>)
+}
