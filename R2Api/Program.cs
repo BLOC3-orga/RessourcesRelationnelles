1280171dde517f63d60b26313e4cc5f0bfa25703
Valentin <valentinwaultier@gmail.com>
2025-04-27 17:00:15 +0200

    Config pour migration (marche pas)

--- R2Api/Program.cs
+++ R2Api/Program.cs
@@ -1,32 +1,34 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using R2Model.Context;
 using R2Model.Entities;
-
+using Microsoft.EntityFrameworkCore.SqlServer;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-var connectionString =
-    builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? throw new InvalidOperationException("Connection string"
-        + "'DefaultConnection' not found.");
+builder.Services.AddDbContext<R2DbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
- .AddIdentityCookies();
+    .AddIdentityCookies();
+    
 builder.Services.AddAuthorizationBuilder();
 
 builder.Services.AddIdentityCore<User>()
- .AddEntityFrameworkStores<R2DbContext>()
- .AddApiEndpoints();
+    .AddEntityFrameworkStores<R2DbContext>()
+    .AddApiEndpoints();
 
-// Configure the HTTP request pipeline.
+var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -34,9 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- R2Model/Context/R2DbContext.cs
+++ R2Model/Context/R2DbContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using R2Model.Entities;
+﻿using R2Model.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
